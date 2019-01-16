490115d9a7c59c9708259ead723bd0d78ae5b9d8
dairuiquan <337959048@qq.com>
2019-01-16 11:27:36 +0800

    修改app.js 请求wns接口

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var wnsRouter = require('./routes/wns');
 var app = express();
 
 // view engine setup
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/wns',wnsRouter);
+app.listen(8090);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
