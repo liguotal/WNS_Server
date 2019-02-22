e799e663272dac8b338ab4cf4f5e61af28f82980
李萌萌 <limengmeng@youxin.com>
2019-02-22 22:59:29 +0800

    no message

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log(JSON.stringify(req.headers)+'ceshi3')
+  console.log(JSON.stringify(req.header)+'ceshi3')
+  console.log(JSON.stringify(req.query)+'ceshi3')
   res.send('respond with a resource');
 });
 router.post('/',function(req, res, next) {
