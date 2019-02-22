bbe52aace50ff9ee6cda4a385e2c3e473d13f364
李萌萌 <limengmeng@youxin.com>
2019-02-22 17:19:54 +0800

    修改内容

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/',function(req, res, next) {
+  console.log(JSON.stringify(req.body)+'ceshi1')
   res.send('respond with a resourcepost2');
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.post('/',function(req, res, next) {
+  console.log(JSON.stringify(req.body)+'ceshi2')
   res.send('respond with a resourcepost');
 });
 module.exports = router;
