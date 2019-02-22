3a855675beb415c2a87e0fd67c179632957a5e01
李萌萌 <limengmeng@youxin.com>
2019-02-22 16:13:57 +0800

    修改配置

--- conf/wnsConst.js
+++ conf/wnsConst.js
@@ -1,8 +1,8 @@
 module.exports = {
-    // secretkey: 'HSZV0Lx11sZJN5wTq6VflmmYMzEA0hQ9',
-    // secretId: 'AKID43nMpCHDSbdbP1FRRNfr5QdlneTvoMUk',
-    // appId: 204085
-    secretkey: 'xr10tEOagga9LZHqJhuwmU1e5K3b3LRL',
-    secretId: 'AKIDZ0XTRJEq47kGftSzKuBLKOcHrdEkMaif',
-    appId: 204099
+    secretkey: 'HSZV0Lx11sZJN5wTq6VflmmYMzEA0hQ9',
+    secretId: 'AKID43nMpCHDSbdbP1FRRNfr5QdlneTvoMUk',
+    appId: 204085
+    // secretkey: 'xr10tEOagga9LZHqJhuwmU1e5K3b3LRL',
+    // secretId: 'AKIDZ0XTRJEq47kGftSzKuBLKOcHrdEkMaif',
+    // appId: 204099
 }
